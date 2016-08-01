0fa37251f162687b5bc303a58fb311258d3f776e
Peter Westmacott <peter.westmacott@gmail.com>
2016-08-01 20:58:56 +0100

    Add text to pre-amble.

--- src/prisoners/pre_amble.cljs
+++ src/prisoners/pre_amble.cljs
@@ -8,30 +8,46 @@
   (reset! state [a b]))
 
 (defn pre-amble []
-  [:div [:h1 "The Prisoner's Dilemma"]
 
-   [:svg {:style    {:width  "50%"
-                     :height "50%"}
-          :view-box "-1 -1 61.5 61.5"}
+  (let [[x0 x1 x2 x3 x4] [0 10 20 40 60]
+        [y0 y1 y2 y3 y4] [0 10 20 40 60]
+        color-a "rgb(255,255,0)"
+        color-b "rgb(255,150,255)"
+        color-c "rgb(100,250,100)"
+        color-d "rgb(250,100,100)"
+        lines "black"
 
-    (let [[x0 x1 x2 x3 x4] [0 10 20 40 60]
-          [y0 y1 y2 y3 y4] [0 10 20 40 60]
-          color-a "rgb(255,255,0)"
-          color-b "rgb(255,150,255)"
-          color-c "rgb(100,250,100)"
-          color-d "rgb(250,100,100)"
-          lines "black"
+        color-a-dim "rgb(75,75,0)"
+        color-b-dim "rgb(75,30,75)"
+        color-c-dim "rgb(20,75,20)"
+        color-d-dim "rgb(75,20,20)"
 
-          color-a-dim "rgb(75,75,0)"
-          color-b-dim "rgb(75,30,75)"
-          color-c-dim "rgb(20,75,20)"
-          color-d-dim "rgb(75,20,20)"
+        [a b] @state
+        a-co-op (not= a "betray")
+        a-betray (not= a "co-op")
+        b-co-op (not= b "betray")
+        b-betray (not= b "co-op")]
 
-          [a b] @state
-          a-co-op (not= a "betray")
-          a-betray (not= a "co-op")
-          b-co-op (not= b "betray")
-          b-betray (not= b "co-op")]
+    [:div
+     [:h1 "The Prisoner's Dilemma"]
+     [:h2 "The Setup"]
+     [:p
+      "You and another person are arrested and given prison sentences. "
+      "You are held in separate cells and cannot communicate with each other. "
+      "The guard gives you both an ultimatum: "
+      [:ul
+       [:li [:span {:style {:color color-d}} "Betray"] " the other prisoner by giving evidence against them and you walk free."]
+       [:li [:span {:style {:color color-c}} "Co-operate"] " with the other prisoner by keeping quiet and serving your current sentence."]]
+      "But:"
+      [:ul
+       [:li "If you both betray each other then you both get a slightly worse sentence."]
+       [:li "If you co-operate and the other prisoner betrays you then you will get the worst possible sentence."]]]
+
+     [:h2 "The Payoffs"]
+
+     [:svg {:style    {:width  "50%"
+                       :height "50%"}
+            :view-box "-1 -1 61.5 61.5"}
 
       [:g {:on-mouse-leave #(dim nil nil)}
 
@@ -124,4 +140,4 @@
                   :y           (- yb 2)
                   :font-family "Verdana"
                   :font-size   "10"
-                  :fill        "black"} bs]])])]])
+                  :fill        "black"} bs]])]]]))
