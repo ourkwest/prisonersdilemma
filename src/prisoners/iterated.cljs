df78879a743072b23c3b8875f08640c49e33c992
Peter Westmacott <peter.westmacott@gmail.com>
2016-08-12 21:37:14 +0100

    Adding remove and replay buttons to the iteration section.

--- src/prisoners/iterated.cljs
+++ src/prisoners/iterated.cljs
@@ -12,13 +12,20 @@
 ;;   all selected strategies should be played against each other (100 rounds?) in a grid showing the final scores
 ;;   user can select the numebr of rounds.
 
-(def alphabetic-strategies (sort-by (comp last last) strategies/strategies))
+(def nbsp "\u00A0")
 
-(def strategies (atom []))
+(defn space [n-before text n-after]
+  (apply str (concat (repeat n-before nbsp) [text] (repeat n-after nbsp))))
 
-(def state (atom {:iterations 100
+(def state (atom {:touch 0
+                  :iterations 100
                   :strategies (sort-by (comp last last) strategies/strategies)}))
 
+(defn remove-strategy [team]
+  (swap! state update :strategies #(remove (fn [[x _]] (= x team)) %)))
+
+(defn re-run []
+  (swap! state update :touch inc))
 
 (defn play-iterations [team-1 team-2 n]
   (let [function-1 (first (strategies/strategies team-1))
@@ -50,7 +57,8 @@
         totals (into {} (for [[team-1 _] strategies]
                           [team-1 (reduce + (vals (get games team-1)))]))
         max-score-per-game (* 5 iterations)
-        smallest-total (* (apply min (vals totals)) 0.95)
+        exageration-factor 0.95
+        smallest-total (* (apply min (vals totals)) exageration-factor)
         largest-total (apply max (vals totals))
         total-range (- largest-total smallest-total)
         ]
@@ -63,59 +71,71 @@
 
       [:table {:style {:border-collapse :collapse}}
 
-       [:tr
-        (concat [[:td {:key "empty-header"}]]
-                (for [[_ [_ color-2 label-2]] strategies]
-                  [:td {:key   (str "header-" label-2)
-                        :style {:background-color color-2
-                                :color            :black
-                                :border-right     (str "1px solid " color-2)
-                                ;:border-radius "50px 50px 0px 0px"
-                                :height           "1em"}} ""])
-                [[:td {:key "total-header"
-                       :style {:background-color :black
-                               :color :white
-                               :padding 5}} "Total Score"]])]
+       [:thead
 
-       (for [[team-1 [_ color-1 label-1]] strategies]
-         [:tr {:key   (str "tr-" label-1)
-               :style {:background-color color-1 :color :black}}
-          (concat [[:td {:key   (str "td-" label-1)
-                         :style {:border-bottom (str "1px solid " color-1)}} label-1]]
-                  (for [[team-2 [_ color-2 label-2]] strategies]
-                    (let [score-1 (get-in games [team-1 team-2])
-                          relative-score (/ score-1 max-score-per-game)
-                          scaled-score (Math/round (* 512 relative-score))
-                          color (str "rgb("
-                                     (min 255 (- 512 scaled-score)) \,
-                                     (min 255 scaled-score) \,
-                                     0 \))]
-                      [:td {:key   (str "td-" label-2 label-1)
-                            :style {:background-color :black
-                                    :border-bottom    (str "1px solid " color-1)
-                                    :border-right     (str "1px solid " color-2)}}
-                       [:div {:style {:background-color color
-                                      :padding 5
-                                      :margin 5
-                                      :text-align :right
-                                      :border-radius 15;
-                                      }}
-                        score-1]]))
+        [:tr
+         (concat [[:td {:key "empty-header-1"}
+                   [:input {:type :button
+                            :value "Re-run"
+                            :on-click re-run}]]
+                  [:td {:key "empty-header-2"}]]
+                 (for [[_ [_ color-2 label-2]] strategies]
+                   [:td {:key   (str "header-" label-2)
+                         :style {:background-color color-2
+                                 :color            :black
+                                 :border-right     (str "1px solid " color-2)
+                                 ;:border-radius "50px 50px 0px 0px"
+                                 :height           "1em"}} ""])
+                 [[:td {:key   "total-header"
+                        :style {:background-color :black
+                                :color            :white
+                                :padding          5}} (space 5 "Total Score" 5)]])]]
 
-                  (let [total (get totals team-1)
-                        relative-total (Math/round (/ (- total smallest-total) total-range 0.01))]
-                    [[:td {:key   (str "total-" label-1)
-                           :style {:border-bottom    (str "1px solid " color-1)
-                                   :background-color :black}}
-                      [:div {:style {:position :relative
-                                     :width "100%"
-                                     :height "2em"}}
-                       [:div {:style {:position         :absolute
-                                      :width            (str relative-total "%")
-                                      :height "100%"
-                                      :background-color color-1}}
-                        " "]]]])
-                  )])]]
+       [:tbody
+        (for [[team-1 [_ color-1 label-1]] strategies]
+          [:tr {:key   (str "tr-" label-1)
+                :style {:background-color color-1 :color :black}}
+           (concat [[:td {:key (str "td-button-" label-1)}
+                     [:input {:type     "button"
+                              :value    "Remove"
+                              :on-click #(remove-strategy team-1)
+                              :style    {:margin 10}}]]
+                    [:td {:key   (str "td-" label-1)
+                          :style {:border-bottom (str "1px solid " color-1)}} label-1]]
+                   (for [[team-2 [_ color-2 label-2]] strategies]
+                     (let [score-1 (get-in games [team-1 team-2])
+                           relative-score (/ score-1 max-score-per-game)
+                           scaled-score (Math/round (* 512 relative-score))
+                           color (str "rgb("
+                                      (min 255 (- 512 scaled-score)) \,
+                                      (min 255 scaled-score) \,
+                                      0 \))]
+                       [:td {:key   (str "td-" label-2 label-1)
+                             :style {:background-color :black
+                                     :border-bottom    (str "1px solid " color-1)
+                                     :border-right     (str "1px solid " color-2)}}
+                        [:div {:style {:background-color color
+                                       :padding          5
+                                       :margin           5
+                                       :text-align       :right
+                                       :border-radius    15 ;
+                                       }}
+                         score-1]]))
+
+                   (let [total (get totals team-1)
+                         relative-total (Math/round (/ (- total smallest-total) total-range 0.01))]
+                     [[:td {:key   (str "total-" label-1)
+                            :style {:border-bottom    (str "1px solid " color-1)
+                                    :background-color :black}}
+                       [:div {:style {:position :relative
+                                      :width    "100%"
+                                      :height   "2em"}}
+                        [:div {:style {:position         :absolute
+                                       :width            (str relative-total "%")
+                                       :height           "100%"
+                                       :background-color color-1}}
+                         " "]]]])
+                   )])]]]
 
      [:span "TODO: finish this bit!"]
 
